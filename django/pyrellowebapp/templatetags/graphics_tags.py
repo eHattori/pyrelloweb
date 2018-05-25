2988c259126fc0b8e1c3366da9a4c826223e8ce5
Leandro Mattoso <lcmattoso@gmail.com>
2018-05-24 23:51:44 -0300

    histogram

--- django/pyrellowebapp/templatetags/graphics_tags.py
+++ django/pyrellowebapp/templatetags/graphics_tags.py
@@ -12,3 +12,31 @@ def graphics_one():
     return {
         'chave_1': testando,
     }
+
+
+@register.simple_tag
+def menu():
+    boards = Board.objects.all()
+    result = []
+    GRAPHICS = [{"label": "Histogram", "link": "histogram"}]
+    for board in boards:
+        menu_item = {"menu": board.name, "submenu": []}
+        for graphic in GRAPHICS:
+
+            menu_item["submenu"].append(
+                    {'label':  graphic["label"],
+                    'link': "boards/%s/%s" % (board.id, graphic["link"])})
+        
+        result.append(menu_item)
+    return result
+
+@register.simple_tag
+def histogram():
+    board = Board.objects.get(id=1)
+    histogram=[]
+    for card in board.card_set.all():
+        leadtime = card.get_leadtime()
+        if leadtime != None:
+            histogram.append(["0", card.get_leadtime()])
+    print(histogram)
+    return histogram
