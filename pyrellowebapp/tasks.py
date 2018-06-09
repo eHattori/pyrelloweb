e18129f30e7e6208aa77b6c6dd6fe2b6076bfa8c
Leandro Mattoso <lcmattoso@gmail.com>
2018-06-09 03:57:16 -0300

    Import jira task added

--- pyrellowebapp/tasks.py
+++ pyrellowebapp/tasks.py
@@ -7,6 +7,11 @@ def import_trello_data():
     call_command('import_trello_data')
 
 @shared_task
+def import_jira_data():
+    from django.core.management import call_command
+    call_command('import_jira_data')
+
+@shared_task
 def graphs_cache():
     from django.core.management import call_command
     call_command('graphs_cache')
